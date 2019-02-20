739d19412d06a43a0c6ab91791c2a410d062355f
Vadim Nifadyev <vnifadyev@gmail.com>
2019-02-20 11:24:44 +0300

    Add parsing input argument
    - Depending on whether flight is two way or not, create correct url for get requests

--- main.py
+++ main.py
@@ -1,33 +1,46 @@
+import argparse
 import requests
-import lxml.etree
 
 
+def main():
+    """ """
 
-# tickets = requests.get("https://apps.penguin.bg/fly/quote3.aspx?ow=&lang=en&depdate=03.07.2019&aptcode1=CPH&aptcode2=BOJ&paxcount=1&infcount=")
-# tickets = requests.get("http://www.flybulgarien.dk/en/search?lang=2&departure-city=CPH&arrival-city=BOJ&reserve-type=&departure-date=26.06.2019&adults-children=1&search=Search%21")
-# print(tickets.status_code)
-# print(tickets.content)
+    argument_parser = argparse.ArgumentParser()
+    # TODO: add description of IATA codes
+    # TODO: create dict with impossible flights combination (also includes the same from to city)
+    argument_parser.add_argument("dep_city", help="departure city IATA code", choices=[
+                                 "CPH", "BLL", "PDV", "BOJ", "SOF", "VAR"])
+    argument_parser.add_argument("dest_city", help="destination city IATA code", choices=[
+                                 "CPH", "BLL", "PDV", "BOJ", "SOF", "VAR"])
+    argument_parser.add_argument("dep_date", help="departure flight date")
+    argument_parser.add_argument(
+        "adults_children", help="Number of adults and children")  # , default="")
+    argument_parser.add_argument(
+        "-return_date", help="return flight date")  # , default="")
+    args = argument_parser.parse_args()
+    print(args)
+    # TODO: do this through exception
+    # TODO: handle cases when there is no return flight
+    # TODO: try to create url through ternary operator
+    if args.return_date:
+        url = "https://apps.penguin.bg/fly/quote3.aspx?rt="\
+        f"&lang=en&depdate={args.dep_date}&aptcode1={args.dep_city}"\
+        f"&rtdate={args.return_date}&aptcode2={args.dest_city}"\
+        f"&paxcount={args.adults_children}&infcount="
+    else:
+        url = "https://apps.penguin.bg/fly/quote3.aspx?ow="\
+        f"&lang=en&depdate={args.dep_date}&aptcode1={args.dep_city}"\
+        f"&aptcode2={args.dest_city}&paxcount={args.adults_children}&infcount="
+    print(url)
+        
+    if requests.get(url).status_code != 200:
+        # TODO: add proper comment
+        raise ValueError("add proper comment")
 
-search_results = requests.get("https://apps.penguin.bg/fly/quote3.aspx?rt=&lang=en&depdate=08.07.2019&aptcode1=BLL&rtdate=15.07.2019&aptcode2=BOJ&paxcount=2&infcount=")
-print(search_results.status_code)
-# print(search_results.text)
-tree = lxml.etree.HTML(search_results.text)
-# TODO: how deep tree is may differ depending from type of flight (return or not)
-# table = tree.xpath("/html/body/form[@id='form1']/div/table[@id='flywiz']")
-# table = tree.xpath("/html/body/form[@id='form1']/div/table[@id='flywiz']/tr/td/table[@id='flywiz_tblQuotes']")
-
-# print(table[0][0][0].text)
-# # print(table.tag)
-# # First and second tr contains table name and names of columns
-# # First and second td contains radio button and style
-# for info in table[0][2]:
-#     print(info.text)
-row = 3
-table = tree.xpath(f"/html/body/form[@id='form1']/div/table[@id='flywiz']/tr/td/table[@id='flywiz_tblQuotes']/tr[{row}]")
-
-print(table)
-print(table[0].tag)
-# First and second tr contains table name and names of columns
-# First and second td contains radio button and style
-for info in table[0]:
-    print(info.text)
+    # TODO: add here main function call (or creating class object)
+# TODO: diff between one way and return flights: ow= and rt=
+# TODO: diff between one way and return flights: no rtdate and rtdate
+# https://apps.penguin.bg/fly/quote3.aspx?ow=&lang=en&depdate=26.06.2019&aptcode1=CPH&aptcode2=BOJ&paxcount=1&infcount=
+# https://apps.penguin.bg/fly/quote3.aspx?rt=&lang=en&depdate=26.06.2019&aptcode1=CPH&rtdate=03.07.2019&aptcode2=BOJ&paxcount=1&infcount=
+if __name__ == "__main__":
+    main()
