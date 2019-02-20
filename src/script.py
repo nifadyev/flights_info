c594c0409deb6d3358e3d7729415c4a0918f8868
Vadim Nifadyev <vnifadyev@gmail.com>
2019-02-20 14:35:58 +0300

    Remove extra arg in function find_flight_info

--- src/script.py
+++ src/script.py
@@ -2,33 +2,20 @@
 import lxml.etree
 
 
-def find_flight_info(request, two_way):
+def find_flight_info(request):
     # TODO: how deep tree is may differ depending from type of flight (return or not)
     tree = lxml.etree.HTML(request.text)
-    # First and second tr contains table name and names of columns
-    # First and second td contains radio button and style
     row = 3  # TODO: check this value for one way flight
     # tbody is not used in xpath (return empty list)
     table = tree.xpath(
         f"/html/body/form[@id='form1']/div/table[@id='flywiz']/tr/td/table[@id='flywiz_tblQuotes']/tr")
     # print(len(table))
-    # print(table[2][1].text)
-    
-    # for info in table[0]:
-    #     print(info.text)
 
+    # First and second table rows contains table name and names of columns
     for row in range(2, len(table)):
         current_row = list()
+        # First and second columns contains radio button and style
         for column in range(1, len(table[row])):
             if table[row][column].text:
                 current_row.append(table[row][column].text)
-            # print(table[row][column].text)
         print(" ".join(current_row))
-        # for item in table[row]:
-        #     print(item.text)
-    # if two_way:
-    #     print("two way")
-
-    # else:
-    #     print("one way")
-
