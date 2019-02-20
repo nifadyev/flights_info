ef15a6aa8a618bda8676ab158a2eca29d9cdff27
Vadim Nifadyev <vnifadyev@gmail.com>
2019-02-20 21:43:36 +0300

    Create 2 functions, fix one case
    - Add functions parse_arguments and create_url.  They are moved from main.py file because they should be internal
    - Fix situation when html page contains additional info about flight (about luggage, for example)

--- src/script.py
+++ src/script.py
@@ -1,6 +1,44 @@
+import argparse
 import lxml.etree
 
-# TODO: add def print info
+
+def parse_arguments(args):
+    """ """
+    date = tuple(int(i) for i in args[2].split("."))
+    print(date)
+    # TODO: add check for current date (if this date is aready past)
+    if date[0] < 1 or date[0] > 31 or date[1] < 1\
+            or date[1] > 12 or date[2] < 2019:
+        # print("error")
+        raise argparse.ArgumentError(args[2], "wrong date")
+    # TODO: add check for equal dest_city and dep_city
+    argument_parser = argparse.ArgumentParser()
+    # TODO: add description of IATA codes
+    # TODO: create dict with impossible flights combination
+    # (also includes the same from to city)
+    argument_parser.add_argument("dep_city",
+                                 help="departure city IATA code",
+                                 choices=["CPH", "BLL", "PDV", "BOJ",
+                                          "SOF", "VAR"])
+    argument_parser.add_argument("dest_city",
+                                 help="destination city IATA code",
+                                 choices=["CPH", "BLL", "PDV", "BOJ",
+                                          "SOF", "VAR"])
+    argument_parser.add_argument("dep_date", help="departure flight date")
+    argument_parser.add_argument("adults_children",
+                                 help="Number of adults and children")
+    argument_parser.add_argument("-return_date", help="return flight date")
+
+    return argument_parser.parse_args(args)
+
+
+def create_url(args):
+    """ """
+    return f"https://apps.penguin.bg/fly/quote3.aspx?"\
+        f"{'rt=' if args.return_date else 'ow='}"\
+        f"&lang=en&depdate={args.dep_date}&aptcode1={args.dep_city}"\
+        f"{f'&rtdate={args.return_date}' if args.return_date else ''}"\
+        f"&aptcode2={args.dest_city}&paxcount={args.adults_children}&infcount="
 
 
 def find_flight_info(request):
@@ -11,9 +49,11 @@ def find_flight_info(request):
         f"/tr/td/table[@id='flywiz_tblQuotes']/tr")
 
     # Table header
-    print("{:<15} {:<10} {:<10} {:<20} {:<20} {:<20}".format(
+    print("{:<17} {:<10} {:<10} {:<20} {:<20} {:<13} {:<21}".format(
         table[1][1].text, table[1][2].text, table[1][3].text,
-        table[1][4].text, table[1][5].text, "Price"))
+        table[1][4].text, table[1][5].text, "Price", "Additional information"))
+    # Price is displayed per person
+    # TODO: maybe change price to total cost or just remove adults arg
 
     # TODO: if data is not available then request will return closest flight
     # First and second table rows contains table name and names of columns
@@ -34,9 +74,11 @@ def find_flight_info(request):
                 current_row.append(table[row][column].text)
 
         if len(current_row) == 5:
-            print("{:<15} {:<10} {:<10} {:<20} {:<21}".format(
+            print("{:<17} {:<10} {:<10} {:<20} {:<21}".format(
                 current_row[0], current_row[1], current_row[2],
                 current_row[3], current_row[4]), end="")
         # Row with price
         elif len(current_row) == 1:
-            print("{:<20}".format(current_row[0][8:]))
+            print("{:<13}".format(current_row[0][8:]))
+        elif len(current_row) == 2:
+            print("{:<13} {:<20}".format(current_row[0][8:], current_row[1]))
