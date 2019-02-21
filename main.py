3c365b2d1e4f250c04a368c64fc4782b16f2c32a
Vadim Nifadyev <vnifadyev@gmail.com>
2019-02-21 15:29:02 +0300

    Remove extra stuff

--- main.py
+++ main.py
@@ -9,23 +9,19 @@ def main():
     args = source.parse_arguments(sys.argv[1:])
     # TODO: do this through exception
     # TODO: handle cases when there is no return flight
-    available_roots = {("CPH", "BOJ"), ("BLL", "BOJ"), ("BOJ", "CPH"),
-                       ("BOJ", "BLL")}
-    if (args.dep_city, args.dest_city) in available_roots:
-        print("Alright")
 
     # TODO: add all available date for each root
     url = source.create_url(args)
-    print(url)
+    # print(url)
 
     request = requests.get(url)
     if request.status_code != 200:
         # TODO: add proper comment
         raise ValueError("add proper comment")
     else:
-        print("Correct request")
+        print("Status code: 200")
 
-    source.find_flight_info(request)
+    source.find_flight_info(request, args)
 
 
 if __name__ == "__main__":
