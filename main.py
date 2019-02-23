559e31581e5597b0f15598cea55193c22d6ce2b9
Vadim Nifadyev <vnifadyev@gmail.com>
2019-02-23 21:17:26 +0300

    Move requests.get inside find_flight_info function

--- main.py
+++ main.py
@@ -1,27 +1,22 @@
 
 import sys
-import requests
 import src.script as source
 
 
 def main():
     """ """
     args = source.parse_arguments(sys.argv[1:])
+    # result = None
+    # while result is None:
+    #     try:
+    #         # connect
+    #         result = get_data(...)
+    #     except:
+    #         pass
+    # # other code that uses result but is not involved in getting it
     # TODO: do this through exception
-    # TODO: handle cases when there is no return flight
 
-    # TODO: add all available date for each root
-    url = source.create_url(args)
-    # print(url)
-
-    request = requests.get(url)
-    if request.status_code != 200:
-        # TODO: add proper comment
-        raise ValueError("add proper comment")
-    else:
-        print("Status code: 200")
-
-    source.find_flight_info(request, args)
+    source.find_flight_info(args)
 
 
 if __name__ == "__main__":
