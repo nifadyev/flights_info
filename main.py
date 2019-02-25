bfad9be0c86fab266cc34a3d0635dd29ab54fa63
Vadim Nifadyev <vnifadyev@gmail.com>
2019-02-25 15:12:04 +0300

    Incapsulate most of the functions to script.py

--- main.py
+++ main.py
@@ -1,22 +1,11 @@
-
 import sys
 import src.script as source
 
 
 def main():
-    """ """
-    args = source.parse_arguments(sys.argv[1:])
-    # result = None
-    # while result is None:
-    #     try:
-    #         # connect
-    #         result = get_data(...)
-    #     except:
-    #         pass
-    # # other code that uses result but is not involved in getting it
-    # TODO: do this through exception
+    """Demonstrate program functional."""
 
-    source.find_flight_info(args)
+    source.find_flight_info(sys.argv[1:])
 
 
 if __name__ == "__main__":
