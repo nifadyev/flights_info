0238c6b4abed28d43328ed4e9c9fa008ff4f0ece
Vadim Nifadyev <vnifadyev@gmail.com>
2019-02-26 18:11:41 +0300

    Add tests for function validate_date

--- test/tests.py
+++ test/tests.py
@@ -2,16 +2,50 @@ import unittest
 import requests
 import src.script as source
 import argparse
-# import main
 import itertools
 
 
 class TestProgram(unittest.TestCase):
     """ """
-    # def test_run_main(self):
+
+    def test_no_errors_with_correct_args(self):
+        try:
+            source.parse_arguments(["CPH", "BOJ", "26.06.2019", "1"])
+        except ValueError:
+            self.fail("myFunc() raised ExceptionType unexpectedly!")
+
+    def test_validate_date_too_input_arg(self):
+        with self.assertRaises(ValueError) as value_error:
+            source.validate_date("23.01.20191")
+            self.assertEqual(
+                value_error.exception.args[0], "Invalid date format")
+
+    def test_validate_date_too_short_arg(self):
+        with self.assertRaises(ValueError) as value_error:
+            source.validate_date("3.11.2019")
+            self.assertEqual(
+                value_error.exception.args[0], "Invalid date format")
+
+    def test_validate_date_invalid_day(self):
+        with self.assertRaises(ValueError) as value_error:
+            source.validate_date("32.06.2019")
+            self.assertEqual(
+                value_error.exception.args[0], "Invalid date format")
+
+    def test_validate_date_invalid_month(self):
+        with self.assertRaises(ValueError) as value_error:
+            source.validate_date("14.o8.2019")
+            self.assertEqual(
+                value_error.exception.args[0], "Invalid date format")
+
+    def test_validate_date_too_invalid_year(self):
+        with self.assertRaises(ValueError) as value_error:
+            source.validate_date("31.12.2020")
+            self.assertEqual(
+                value_error.exception.args[0], "Invalid date format")
 
     def test_check_all_unavailable_routes(self):
-        args = ["PDV", "BOJ", "26.06.2019", "1"]
+        # args = ["PDV", "BOJ", "26.06.2019", "1"]
         AVAILABLE_ROUTES = (("CPH", "BOJ"), ("BLL", "BOJ"), ("BOJ", "CPH"),
                             ("BOJ", "BLL"))
         CITY_CODES = ("CPH", "BLL", "PDV", "BOJ", "SOF", "VAR")
@@ -20,11 +54,10 @@ class TestProgram(unittest.TestCase):
             if (dep_city, dest_city) not in AVAILABLE_ROUTES:
                 with self.subTest((dep_city, dest_city)):
                     with self.assertRaises(ValueError) as e:
-                        source.parse_arguments(
-                            [f"{dep_city}", f"{dest_city}", "26.06.2019", "1"])
+                        source.check_route(
+                            f"{dep_city}", f"{dest_city}")
                     self.assertEqual(
-                        e.exception.args[0], f"Flight route {dep_city}-{dest_city} is unavailable")
-
+                        e.exception.args[0], f"Unavailable route")
 
     def test_parse_arguments(self):
         """ """
@@ -35,36 +68,14 @@ class TestProgram(unittest.TestCase):
 
         self.assertEqual(source.parse_arguments(input_args), args)
 
-    def test_check_date(self):
-        with self.assertRaises(ValueError) as e:
-            source.parse_arguments(["CPH", "PDV", "26.06.2019", "1"])
+    # def test_check_date(self):
+    #     with self.assertRaises(ValueError) as e:
+    #         source.parse_arguments(["CPH", "PDV", "26.06.2019", "1"])
 
-        # e.exception.args[0] contains error message
-        self.assertEqual(e.exception.args[0],
-                         "Flight route CPH-PDV is unavailable")
-
-    def test_run_test(self):
-        try:
-            source.parse_arguments(["CPH", "BOJ", "26.06.2019", "1"])
-        except ValueError:
-            self.fail("myFunc() raised ExceptionType unexpectedly!")
+    #     # e.exception.args[0] contains error message
+    #     self.assertEqual(e.exception.args[0],
+    #                      "Flight route CPH-PDV is unavailable")
 
 
-    # def test_check_root(self):
-    #     """ """
-    #     args = argparse.Namespace(adults_children='1', dep_city='CPH',
-    #                               dep_date='26.06.2019', dest_city='BOJ',
-    #                               return_date=None)
-    #     input_args = ["CPH", "BOJ", "26.06.2019", "1"]
-    #     city_codes = ["CPH", "BLL", "PDV", "BOJ", "SOF", "VAR"]
-    #     all_roots = itertools.permutations(city_codes, r=2)
-    #     # print(all_roots)
-    #     count = 0
-    #     for i, j in itertools.permutations(city_codes, r=2):
-    #         input_args[0] = i
-    #         input_args[1] = j
-    #         with self.subTest(i=count):
-    #             self.assertEqual(source.parse_arguments(input_args), args)
-    #         count += 1
 if __name__ == '__main__':
     unittest.main()
