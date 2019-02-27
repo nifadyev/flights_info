a7dbc05b9435f55229c15f0e921677821687117c
Vadim Nifadyev <vnifadyev@gmail.com>
2019-02-27 12:35:05 +0300

    Add some tests for check_date_availability

--- test/tests.py
+++ test/tests.py
@@ -80,8 +80,81 @@ class TestProgram(unittest.TestCase):
             self.assertEqual(
                 value_error.exception.args[0], "Old date")
 
+    def test_check_date_availability_correct_cph_to_boj_one_way(self):
+
+        CPH_TO_BOJ_DATES = {"26.06.2019", "03.07.2019", "10.07.2019",
+                            "17.07.2019", "24.07.2019", "31.07.2019",
+                            "07.08.2019"}
+        for date in CPH_TO_BOJ_DATES:
+            args = argparse.Namespace(dep_city="CPH", dest_city="BOJ",
+                                      dep_date=date,
+                                      adults_children="1",
+                                      return_date=None)
+            with self.subTest(date):
+                try:
+                    source.check_date_availability(args, False)
+                except ValueError:
+                    self.fail("check_date_availability raised"
+                              "ValueError unexpectedly!")
+
+    def test_check_date_availability_correct_cph_to_boj_two_way(self):
+        CPH_TO_BOJ_DATES = {"26.06.2019", "03.07.2019", "10.07.2019",
+                            "17.07.2019", "24.07.2019", "31.07.2019",
+                            "07.08.2019"}
+        BOJ_TO_CPH_DATES = {"27.06.2019", "04.07.2019", "11.07.2019",
+                            "18.07.2019", "25.07.2019", "01.08.2019",
+                            "08.08.2019"}
+        for dep_date in CPH_TO_BOJ_DATES:
+            for return_date in BOJ_TO_CPH_DATES:
+                args = argparse.Namespace(dep_city="CPH", dest_city="BOJ",
+                                          dep_date=dep_date,
+                                          adults_children="1",
+                                          return_date=return_date)
+                with self.subTest((dep_date, return_date)):
+                    try:
+                        source.check_date_availability(args, True)
+                    except ValueError:
+                        self.fail("check_date_availability raised"
+                                  "ValueError unexpectedly!")
+
+    def test_check_date_availability_correct_boj_to_bll_one_way(self):
+        BOJ_TO_BLL_DATES = {"01.07.2019", "08.07.2019", "15.07.2019",
+                            "22.07.2019", "29.07.2019", "05.08.2019"}
+
+        for date in BOJ_TO_BLL_DATES:
+            args = argparse.Namespace(dep_city="BOJ", dest_city="BLL",
+                                      dep_date=date,
+                                      adults_children="1",
+                                      return_date=None)
+            with self.subTest(date):
+                try:
+                    source.check_date_availability(args, False)
+                except ValueError:
+                    self.fail("check_date_availability raised"
+                              "ValueError unexpectedly!")
+
+    def test_check_date_availability_correct_boj_to_boj_two_way(self):
+        CPH_TO_BOJ_DATES = {"26.06.2019", "03.07.2019", "10.07.2019",
+                            "17.07.2019", "24.07.2019", "31.07.2019",
+                            "07.08.2019"}
+        BOJ_TO_CPH_DATES = {"27.06.2019", "04.07.2019", "11.07.2019",
+                            "18.07.2019", "25.07.2019", "01.08.2019",
+                            "08.08.2019"}
+        for dep_date in CPH_TO_BOJ_DATES:
+            for return_date in BOJ_TO_CPH_DATES:
+                args = argparse.Namespace(dep_city="CPH", dest_city="BOJ",
+                                          dep_date=dep_date,
+                                          adults_children="1",
+                                          return_date=return_date)
+                with self.subTest((dep_date, return_date)):
+                    try:
+                        source.check_date_availability(args, True)
+                    except ValueError:
+                        self.fail("check_date_availability raised"
+                                  "ValueError unexpectedly!")
+
     def test_check_all_unavailable_routes(self):
-        # args = ["PDV", "BOJ", "26.06.2019", "1"]
+            # args = ["PDV", "BOJ", "26.06.2019", "1"]
         AVAILABLE_ROUTES = (("CPH", "BOJ"), ("BLL", "BOJ"), ("BOJ", "CPH"),
                             ("BOJ", "BLL"))
         CITY_CODES = ("CPH", "BLL", "PDV", "BOJ", "SOF", "VAR")
