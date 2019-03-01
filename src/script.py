cff45523481427e68a48eab56c66d25a24a90717
Vadim Nifadyev <36514612+nifadyev@users.noreply.github.com>
2019-03-01 17:09:16 +0300

    v0.6
    * Shorten code base, fix calculate_flight_duration, add more exception handlers
    * Change fucntion validate_date to check only 1 date instead of 2
    * Add lots of improvements
    - Simplify check_date_availability. Remove extra if statements, move all available flight dates to one structure.
    - Add invalid argument handler in parse_arguments
    - Move data validation in find_flight_info to separate function validate_input_data
    
    * Remove extra if statement in validate_date
    * Add new function to parse city codes
    * Add tests for function validate_date
    * Add more tests for validate_date
    * Fix lots of issues
    - Reimagine validate_city_code. Now it raises ValueError and doesn't return anything
    - Remove choice and type  of arguments dep_city and dest_city in parse_arguments. They are checked in validate_city_code
    - Remove create_url and replace it with parse_url_parameters. It creates valid parameters for requests.get function
    - Remove try-except blocks from validate_input_data because they are unnecessary
    
    * Add some tests for check_date_availability
    * Sort functions by name, add draft for new find_flight_info
    * Divide find_flights_info into 2 functions, handle input argument using type=
    - New function write_flight_information write data into dict
    - Second new function, print_flights_information, output all data about flights to console
    - Again add handling invalid arguments by using type in argparse.add_argument
    
    * Replace date parsing with datetime module methods, add docstrings
    * Major changes in some functions, add current tasks
    - Calculate_flight_duration now uses datetime methods
    - Move DATES to global environment and remove ROUTES cause of its duplicity
    - Remove duplicity in parse_url_arguments
    - validate_date now uses datetime methods
    - Add lots of current tasks to do
    
    * Major improvements
    - Merge check_date_availabilty to check_route. Also add check for old departure date in comparison with return date
    - Simplify find_flights_info by reimagining main for cycle
    - Perform dates validation in parse_arguments using type argument in method add_argument
    - Improve docstrings
    
    * Minor improvements
    - Update docstring for most functions
    - Improve check_route
    - Remove validate_input_data
    - Use datetime methods in validate_date

--- src/script.py
+++ src/script.py
@@ -3,89 +3,84 @@ import datetime
 import lxml.etree
 import lxml.objectify
 import requests
-import collections
 
 # TODO: decorate main func flight_info by check_args and create_url
-# TODO: output info if one of the args data or return date is invalid
+# TODO: change names going out and coming back to outbound and inbound
 
 
 def calculate_flight_duration(departure_time, arrival_time):
     """Return flight duration in format hh:mm."""
 
-    parsed_departure_time = tuple(int(i) for i in departure_time.split(":"))
-    parsed_arrival_time = tuple(int(i) for i in arrival_time.split(":"))
+    dep_time = datetime.datetime.strptime(departure_time, "%H:%M")
+    arr_time = datetime.datetime.strptime(arrival_time, "%H:%M")
+    duration = datetime.timedelta(hours=arr_time.hour - dep_time.hour,
+                                  minutes=arr_time.minute - dep_time.minute)
 
-    minutes = parsed_arrival_time[1] - parsed_departure_time[1]
-    hours = parsed_arrival_time[0] - parsed_departure_time[0]
-    correct_minutes = (60 + minutes) if minutes < 0 else minutes
-    correct_hours = (hours - 1) if minutes < 0 else hours
-
-    duration = [str(correct_hours) if correct_hours >= 10
-                else f"0{correct_hours}",
-                str(correct_minutes) if correct_minutes >= 10
-                else f"0{correct_minutes}"]
-
-    return ":".join(duration)
+    # Remove seconds from output
+    return str(duration)[:-3]
 
 
-def check_date_availability(args, is_return_flight):
-    """Check flight availability for specified date."""
-
-    # Available flight dates for each possible route
-    DATES = {("CPH", "BOJ"): {"26.06.2019", "03.07.2019", "10.07.2019",
-                              "17.07.2019", "24.07.2019", "31.07.2019",
-                              "07.08.2019"},
-             ("BOJ", "CPH"): {"27.06.2019", "04.07.2019", "11.07.2019",
-                              "18.07.2019", "25.07.2019", "01.08.2019",
-                              "08.08.2019"},
-             ("BOJ", "BLL"): {"01.07.2019", "08.07.2019", "15.07.2019",
-                              "22.07.2019", "29.07.2019", "05.08.2019"},
-             ("BLL", "BOJ"): {"01.07.2019", "08.07.2019", "15.07.2019",
-                              "22.07.2019", "29.07.2019", "05.08.2019"}}
-
-    departure_city = args.dest_city if is_return_flight else args.dep_city
-    destination_city = args.dep_city if is_return_flight else args.dest_city
-    date = args.return_date if is_return_flight else args.dep_date
-
-    # Check dates
-    if date not in DATES[(departure_city, destination_city)]:
-        raise ValueError(f"No available flights for date {date}")
+# All available routes and dates
+DATES = {("CPH", "BOJ"): {"26.06.2019", "03.07.2019", "10.07.2019",
+                          "17.07.2019", "24.07.2019", "31.07.2019",
+                          "07.08.2019"},
+         ("BOJ", "CPH"): {"27.06.2019", "04.07.2019", "11.07.2019",
+                          "18.07.2019", "25.07.2019", "01.08.2019",
+                          "08.08.2019"},
+         ("BOJ", "BLL"): {"01.07.2019", "08.07.2019", "15.07.2019",
+                          "22.07.2019", "29.07.2019", "05.08.2019"},
+         ("BLL", "BOJ"): {"01.07.2019", "08.07.2019", "15.07.2019",
+                          "22.07.2019", "29.07.2019", "05.08.2019"}}
 
 
-def check_route(dep_city, dest_city):
-    """Check current route for flight availability.
-    Also check if departure city and destination city are equal.
+def check_route(args):
+    """Check current route for availability.
+
+    Raise KeyError if route is not in DATES.
+    Raise ValueError if departure date is in the past. 
     """
 
-    ROUTES = {("CPH", "BOJ"), ("BLL", "BOJ"), ("BOJ", "CPH"),
-              ("BOJ", "BLL")}
+    if args.return_date\
+        and datetime.datetime.strptime(args.dep_date, "%d.%m.%Y")\
+            > datetime.datetime.strptime(args.return_date, "%d.%m.%Y"):
 
-    if dep_city == dest_city:
-        raise ValueError("Same departure city and destination city")
-    elif (dep_city, dest_city) not in ROUTES:
-        raise ValueError("Unavailable route")
+        raise ValueError("Departure date is in the past"
+                         "in comparison with return date")
+
+    if (args.dep_city, args.dest_city) not in DATES:
+        raise KeyError("Unavailable route")
+
+    # ? whats better: 3 if statements or 1 if but 8 lines of code
+    if args.return_date:
+        departure_city = args.dest_city
+        destination_city = args.dep_city
+        date = args.return_date
+    else:
+        departure_city = args.dep_city
+        destination_city = args.dest_city
+        date = args.dep_date
+
+    if date not in DATES[(departure_city, destination_city)]:
+        raise KeyError(f"No available flights for chosen dates")
 
 
 def find_flight_info(arguments):
-    """Searcg for available flights and output found info to console."""
+    # TODO: Modify docstring
+    """Search for available flights and output found info to console."""
 
-    args = validate_input_data(arguments)
+    args = parse_arguments(arguments)
+    check_route(args)
     request = requests.get("https://apps.penguin.bg/fly/quote3.aspx?",
                            params=parse_url_parameters(args))
-    # Full html page code
+
     if request.text:
         tree = lxml.etree.HTML(request.text)
     else:
         raise ValueError("Request body is empty")
-    # root = lxml.objectify.XML(request.content)
-    # TODO: read about diff between HTML and Objectify
 
     table = tree.xpath(
         "/html/body/form[@id='form1']/div/table[@id='flywiz']"
         "/tr/td/table[@id='flywiz_tblQuotes']/tr")
-
-    # Flight = collections.namedtuple("Flight", "Date, Departure, Arrival,\
-    # Flight duration, From, To, Price, Additional information")
 
     flights_data = {"Going Out": {"Date": "", "Departure": "",
                                   "Arrival": "", "Flight duration": "",
@@ -96,41 +91,48 @@ def find_flight_info(arguments):
                                     "From": "", "To": "", "Price": "",
                                     "Additional information": ""}}
 
-    # TODO: maybe change price to total cost or just remove adults arg
-    # First and second table rows contains table name and names of columns
-    for row in range(2, len(table)):
-        # Empty row, with price, with price and additional info or with table2
-        # header or with table2 name
-        if 1 <= len(table[row]) <= 3 or table[row][1].text == "Date":
+    main_info = list()
+    outbound_flight = False
+    for row in table:
+        if 1 <= len(row) <= 3 or row[1].text == "Date":
+            if len(row) == 3 and main_info:
+                extra_info = (row[1].text[8:] if row[1].text else "",
+                              row[2].text if row[2].text else "")
+                write_flight_information(flights_data["Going Out"
+                                                      if outbound_flight
+                                                      else "Coming back"],
+                                         main_info, extra_info)
+                main_info = list()
             continue
 
         # ? Could it be simplified
+        # row[1].text[:6] contains weekday
         flight_date = datetime.datetime.strptime(
-            table[row][1].text[5:], "%d %b %y").strftime("%d.%m.%Y")
+            row[1].text[5:], "%d %b %y").strftime("%d.%m.%Y")
 
         # Search for going out flight
-        # table[row][4:5].text contains city name and city code => in
+        # row[4:5].text contains city name and city code => in
         if flight_date == args.dep_date\
-                and args.dep_city in table[row][4].text\
-                and args.dest_city in table[row][5].text:
-            write_flight_information(
-                flights_data["Going Out"], table[row], table[row+1])
-            # going_out = Flight()
-            # write_flight_information(
-            #     Flight, table[row], table[row+1])
+                and args.dep_city in row[4].text\
+                and args.dest_city in row[5].text:
 
+            main_info = tuple(item.text for item in row)
+            outbound_flight = True
         elif flight_date == args.return_date\
-                and args.dest_city in table[row][4].text\
-                and args.dep_city in table[row][5].text:
-            write_flight_information(
-                flights_data["Coming back"], table[row], table[row+1])
+                and args.dest_city in row[4].text\
+                and args.dep_city in row[5].text:
+
+            main_info = tuple(item.text for item in row)
+            outbound_flight = False
 
     print_flights_information(flights_data)
 
 
 def parse_arguments(args):
-    """Parse command line arguments using argparse.
-    Contain help message.
+    """Handle command line arguments.
+
+    Raise ArgumentError if argument's type is incorrect.
+    Return valid arguments.
     """
 
     argument_parser = argparse.ArgumentParser(description="Flight informer")
@@ -141,112 +143,97 @@ def parse_arguments(args):
     argument_parser.add_argument("dest_city",
                                  help="destination city IATA code",
                                  type=validate_city_code)
-    argument_parser.add_argument("dep_date", help="departure flight date",)
+    argument_parser.add_argument("dep_date", help="departure flight date",
+                                 type=validate_date)
+    # Probably deprecated => get rid of this arg globally
     argument_parser.add_argument("adults_children",
                                  help="Number of adults and children")
-    argument_parser.add_argument("-return_date", help="return flight date")
+    argument_parser.add_argument("-return_date", help="return flight date",
+                                 type=validate_date)
 
     return argument_parser.parse_args(args)
 
 
 def parse_url_parameters(args):
-    """Return valid url parameters for making a request
-     to Flybulgarien booking engine.
-     """
+    """Return valid url parameters for requests.get method."""
 
-    if args.return_date:
-        return {"rt": "", "lang": "en", "depdate": args.dep_date,
-                "aptcode1": args.dep_city, "rtdate": args.return_date,
-                "aptcode2": args.dest_city, "paxcount": args.adults_children}
+    params = {"rt" if args.return_date else "ow": "", "lang": "en",
+              "depdate": args.dep_date,
+              "aptcode1": args.dep_city, "rtdate": args.return_date,
+              "aptcode2": args.dest_city, "paxcount": args.adults_children}
 
-    return {"ow": "", "lang": "en", "depdate": args.dep_date,
-            "aptcode1": args.dep_city, "aptcode2": args.dest_city,
-            "paxcount": args.adults_children}
+    if not args.return_date:
+        del params["rtdate"]
 
-# TODO: add description of IATA codes
+    return params
 
 
 def print_flights_information(flights_info):
-    """Output table containing inforamtion about flights."""
+    """Print information about flights."""
 
     # Table header
     print("{:<12} {:<17} {:<10} {:<10} {:<15} {:<20} {:<20} {:<13} {:<21}\
         ".format("Direction", *flights_info["Going Out"].keys()))
-
+    # Outbound flight
     print("{:<12} {:<17} {:<10} {:<10} {:<15} {:<20} {:<20} {:<13} {:<20}\
         ".format("Going Out", *flights_info["Going Out"].values()))
+    # Inbound flight
     if flights_info["Coming back"]["Date"]:
         print("{:<12} {:<17} {:<10} {:<10} {:<15} {:<20} {:<20} {:<13} {:<20}\
             ".format("Coming back", *flights_info["Coming back"].values()))
 
 
 def validate_city_code(code):
-    """Return city code if it is valid.
-    Raise ArgumentError otherwise.
+    """Raise ArgumentError if city code isn't in VALID_CITY_CODES.
+
+    Return city code.
     """
 
-    VALID_CODES = {"CPH", "BLL", "PDV", "BOJ", "SOF", "VAR"}
+    VALID_CITY_CODES = {"CPH", "BLL", "PDV", "BOJ", "SOF", "VAR"}
 
-    if code not in VALID_CODES:
+    if code not in VALID_CITY_CODES:
         raise argparse.ArgumentTypeError("Invalid city code")
 
     return code
 
 
 def validate_date(flight_date):
-    """Raise ValueError if date is invalid or in the past."""
+    """Raise ValueError if date is invalid or in the past.
 
-    # ? Why reversed is needed
-    parsed_date = tuple(int(i) for i in flight_date.split("."))
-    # current date format: dd.mm.yyyy
+    Return valid date.
+    """
 
-    try:
-        datetime.date(*reversed(parsed_date)).strftime("%d.%m.%Y")
-    except ValueError as value_error:
-        raise ValueError(f"{value_error.args[0]}. Please enter valid date")
+    parsed_dates = datetime.datetime.strptime(
+        flight_date, "%d.%m.%Y")
 
-    if datetime.date.today() > datetime.date(*reversed(parsed_date)):
+    if datetime.datetime.today() > parsed_dates:
         raise ValueError("Date in the past")
 
+    return flight_date
 
-def validate_input_data(arguments):
-    """ Validate all input arguments.
-    Raise exceptions if they are invalid.
 
-    Return parsed arguments. 
+def write_flight_information(flights_data, route_info, extra_route_info):
+    """Write flight information to flights_data data structure (dict).
+
+    Keyword arguments:
+        flights_data -- dictionary with ready to print flights information.
+        route_info -- main information about current route.
+        extra_route_info -- price and additional information about route.
     """
-    # Check city codes
-    # FIXME: argparse throw an error. How to avoid this?
-    args = parse_arguments(arguments)
-    validate_date(args.dep_date)
-    check_date_availability(args, False)
-    # Check route for availability
-    check_route(args.dep_city, args.dest_city)
-    if args.return_date:
-        validate_date(args.return_date)
-        check_date_availability(args, True)
 
-    return args
-
-
-def write_flight_information(dest, flight_info, extra_flight_info):
-    """Write flight information to dest data structure (dict)."""
-
+    # TODO: rename i or get rid of it
     i = 1
-    # TODO: create list with result values and unpack it in Flight namedtuple
-    # Flight._make(iterable)
-    for key in dest:
+    for key in flights_data:
         if key == "Flight duration":
-            dest[key] = calculate_flight_duration(
-                flight_info[2].text, flight_info[3].text)
+            flights_data[key] = calculate_flight_duration(
+                route_info[2], route_info[3])
         # Price is displayed per person
-        elif key == "Price" and extra_flight_info[1].text:
+        elif key == "Price" and extra_route_info[0]:
             # table[row+1][1].text[0:9] = "Price = "
-            dest[key] = extra_flight_info[1].text[8:]
-        # Row + 1 also contains additional information
-        # eg. NO_LUGGAGE_INCLUDED
-        elif key == "Additional information" and extra_flight_info[2].text:
-            dest[key] = extra_flight_info[2].text
+            flights_data[key] = extra_route_info[0]
+        # Row+1 also contains additional information (eg. NO_LUGGAGE_INCLUDED)
+        elif key == "Additional information" and extra_route_info[1]:
+            flights_data[key] = extra_route_info[1]
         elif i < 6:
-            dest[key] = flight_info[i].text if flight_info[i].text else ""
+            flights_data[key] = route_info[i] if route_info[i] else ""
             i += 1
