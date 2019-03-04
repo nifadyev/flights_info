f314ded53ef2c5248d63cfe078664bce3550edf2
Vadim Nifadyev <vnifadyev@gmail.com>
2019-03-04 21:24:03 +0300

    Improve find_flight_info and write_flight_information
    - find_flight_info: shorten flights_data dict, merge main_info and extra_info into flight_info
    - print_flights_information: change check for return flight
    - validare_persons: add try-catch block for arg type check
    - write_flight_inforamtion: change arguments, docstring, add return value - dict with parsed flight info

--- src/script.py
+++ src/script.py
@@ -6,6 +6,7 @@ import requests
 # TODO: Handle cases where there is no flight info
 # TODO: find it in prev commits (case: persons= -2)
 # TODO: define custom exception (optional)
+# TODO: globally change docstrings to the one in write...
 
 # All available routes and dates
 DATES = {("CPH", "BOJ"): {"26.06.2019", "03.07.2019", "10.07.2019",
@@ -78,28 +79,20 @@ def find_flight_info(arguments):
         "/html/body/form[@id='form1']/div/table[@id='flywiz']"
         "/tr/td/table[@id='flywiz_tblQuotes']/tr")
 
-    # TODO: handle this: simplify or move or write directly to func
-    flights_data = {"Outbound": {"Date": "", "Departure": "",
-                                 "Arrival": "", "Flight duration": "",
-                                 "From": "", "To": "", "Price": "",
-                                 "Additional information": ""},
-                    "Inbound": {"Date": "", "Departure": "",
-                                "Arrival": "", "Flight duration": "",
-                                "From": "", "To": "", "Price": "",
-                                "Additional information": ""}}
+    flights_data = {"Outbound": 0, "Inbound": 0}
 
-    main_info = list()
-    outbound_flight = False
     for row in table:
         if 1 <= len(row) <= 3 or row[1].text == "Date":
-            if len(row) == 3 and main_info:
-                # TODO: Write extra_info to flights_data or remove this fields
-                extra_info = (row[1].text[8:] if row[1].text else "",
-                              row[2].text if row[2].text else "")
+            if len(row) == 3 and flight_info:
+                # row[1].text[:9]="Price = "
+                flight_info.append(row[1].text[8:] if row[1].text else "")
+                flight_info.append(row[2].text if row[2].text else "")
                 direction = "Outbound" if outbound_flight else "Inbound"
-                write_flight_information(flights_data[direction],
-                                         main_info, extra_info, args.persons)
-                main_info = list()
+
+                flights_data[direction] = write_flight_information(
+                    flight_info, args.persons)
+
+                flight_info = None
             continue
 
         # row[1].text[:6] contains weekday
@@ -111,13 +104,13 @@ def find_flight_info(arguments):
                 and args.dep_city in row[4].text\
                 and args.dest_city in row[5].text:
 
-            main_info = tuple(item.text for item in row)
+            flight_info = [item.text for item in row]
             outbound_flight = True
         elif flight_date == args.return_date\
                 and args.dest_city in row[4].text\
                 and args.dep_city in row[5].text:
 
-            main_info = tuple(item.text for item in row)
+            flight_info = [item.text for item in row]
             outbound_flight = False
 
     return flights_data
@@ -149,7 +142,6 @@ def parse_arguments(args):
     # ? what's better: pretty error message by argparse but ugly test output
     # ? or full exception traceback but pretty test output
     def raise_value_error(err_msg):
-        # raise SystemExit(err_msg)
         raise argparse.ArgumentTypeError(err_msg)
 
     argument_parser.error = raise_value_error
@@ -183,7 +175,7 @@ def print_flights_information(flights_info):
     print("{:<12} {:<17} {:<10} {:<10} {:<15} {:<20} {:<20} {:<13} {:<20}\
         ".format("Outbound", *flights_info["Outbound"].values()))
     # Inbound flight
-    if flights_info["Inbound"]["Date"]:
+    if flights_info["Inbound"]:
         total_cost = int(flights_info["Outbound"]["Price"][:-7])\
             + int(flights_info["Inbound"]["Price"][:-7])
 
@@ -218,46 +210,54 @@ def validate_date(flight_date):
     if datetime.datetime.today() > parsed_date:
         raise ValueError("Date in the past")
 
-    # return parsed_date.strftime("%d.%m.%Y")
-
     return parsed_date
 
 
 def validate_persons(persons):
     """ """
+    try:
+        valid_persons = int(persons)
+    except ValueError:
+        pass
 
-    if not 0 < int(persons) < 10:
+    if not 0 < valid_persons < 10:
         raise argparse.ArgumentTypeError("Invalid persons number. "
                                          "Max persons number: 9")
 
-    # return int(persons)
-    return persons
+    return valid_persons
 
 
-def write_flight_information(flights_data, route_info, extra_route_info,
-                             persons):
-    """Write flight information to flights_data data structure (dict).
+def write_flight_information(raw_flight_info, persons):
+    """Parse flight information.
 
-    Keyword arguments:
-        flights_data -- dictionary with ready to print flights information.
-        route_info -- main information about current route.
-        extra_route_info -- price and additional information about route.
-        persons -- total number of adults and children.
+    Arguments:
+        raw_flight_info {list} -- parsed from booking engine data.
+        persons {int} -- total number of people to book.
+
+    Returns:
+        dict -- parsed flight information.
     """
 
+    filled_flight_info = {"Date": "", "Departure": "",
+                          "Arrival": "", "Flight duration": "",
+                          "From": "", "To": "", "Price": "",
+                          "Additional information": ""}
     i = 1
 
-    for key in flights_data:
+    for key in filled_flight_info:
         if key == "Flight duration":
-            flights_data[key] = calculate_flight_duration(
-                route_info[2], route_info[3])
-        elif key == "Price" and extra_route_info[0]:
+            filled_flight_info[key] = calculate_flight_duration(
+                raw_flight_info[2], raw_flight_info[3])
+        elif key == "Price" and raw_flight_info[-2]:
             # extra_route_info[0][-6:] contains currency
-            total_cost = int(extra_route_info[0][:-7]) * int(persons)
-            flights_data[key] = f"{total_cost}.00 EUR"
+            total_cost = int(raw_flight_info[-2][:-7]) * persons
+            filled_flight_info[key] = f"{total_cost}.00 EUR"
         # Row+1 also contains additional information (eg. NO_LUGGAGE_INCLUDED)
-        elif key == "Additional information" and extra_route_info[1]:
-            flights_data[key] = extra_route_info[1]
+        elif key == "Additional information" and raw_flight_info[-1]:
+            filled_flight_info[key] = raw_flight_info[-1]
         elif i < 6:
-            flights_data[key] = route_info[i] if route_info[i] else ""
+            filled_flight_info[key] = raw_flight_info[i]\
+                if raw_flight_info[i] else ""
             i += 1
+
+    return filled_flight_info
