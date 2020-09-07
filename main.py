3abd37d06abdb67d214414b2e2bc196e28767a74
Vadim Nifadyev <vnifadyev@gmail.com>
2020-09-07 20:44:37 +0300

    Replace double quotes with single quotes

--- main.py
+++ main.py
@@ -4,9 +4,8 @@ import src.script as source
 
 def main():
     """Flights_info demo."""
-
     source.print_flights_information(source.find_flight_info(sys.argv[1:]))
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
